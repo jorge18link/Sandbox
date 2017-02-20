37bd38222ec2db751af75c7623e0ebd497460cd0
José Luis <Jos Luis>
2017-02-19 20:03:33 -0500

    cambio de repositorio por algun error en el anterior

---
+++ Sandbox/models/usuario.js
@@ -0,0 +1,42 @@
+var mongoose= require('mongoose');
+var bcrypt = require('bcrypt');
+var Schema = require('mongoose').Schema
+
+var usuarioSchema = new Schema({
+   CorreoElectronico:String,
+   Contrasena:String,
+   Rol:String,
+   TipodeIdentificacion:String,
+   Identificacion:String,
+   Nombres:String,
+   Apellidos:String,
+   Carrera:String
+})
+
+var User=module.exports=mongoose.model('usuario',usuarioSchema);
+
+module.exports.getUserByUsername = function(username, callback){
+	var query = {CorreoElectronico: username};
+    User.findOne(query, callback);
+    
+}
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	console.log(hash)
+    console.log(candidatePassword)
+    var salt = bcrypt.genSaltSync(10);
+    bcrypt.hash(hash, salt, function(err, hash) {
+        if (err) { throw (err); 
+        }
+         bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+            if (err) { throw (err); }
+            callback(null, isMatch);
+            console.log('Ver isMatch');
+            console.log(isMatch);
+     });
+    });
+}
