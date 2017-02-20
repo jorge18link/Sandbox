512add64f8023b91744a6b0048bf177d5284136c
jorge18link <18linkjorge@gmail.com>
2017-02-20 00:59:59 -0500

    creadas las api de eliminar y de leer

--- Sandbox/routes/users.js
+++ Sandbox/routes/users.js
@@ -4,25 +4,31 @@ var router = express.Router();
 var User=require('../models/usuario');
 
 router.get('/', function(req, res){
-	res.render('prueba')
+	User.find({}, function(err, users) {
+    if(err) throw err;
+		res.send(users);
+	});
 });
 
-router.post('/modificar', function(req,res){
+router.get('/prueba', function(req, res){
+	res.render('prueba');
+});
+
+
+router.post('/modificar',ensureAuthenticated, function(req,res){
 		correo = req.body.correo
 		//query de busqueda
 		var busca = {CorreoElectronico: correo};
-		
 		/*
 		User.findOne(busca,function(err, user){
 			console.log('Usuario Anterior:');
 			console.log(user);
 		});
 		*/
-
 		//Se toman los datos de la vista con los parametros que vas a modificar
 		var modificacion = {
 			Rol: req.body.rol,
-   		TipoIdentificacion: req.body.tipoIdentificacion,
+   		TipodeIdentificacion: req.body.tipoIdentificacion,
    		Identificacion: req.body.identificacion,
    		Nombres: req.body.nombres,
    		Apellidos: req.body.apellidos,
@@ -35,20 +41,34 @@ router.post('/modificar', function(req,res){
 					console.log("Something wrong when updating data!");
 			}
 			console.log('Usuario Modificado con exito');
+			//console.log(doc);
 		});
 
 		//Aqui Carga la pagina que sigue
-		res.send("sw")
-
+		res.redirect('/');
 });
 
+router.post('/eliminar',ensureAuthenticated, function(req,res){
+		correo = req.body.correo
+		var busca = {CorreoElectronico: correo};
+
+		User.findOne(busca,function(err, user){
+			if(err) throw err;
+			if(!user){
+				return done(null, false, {message: 'Unknown User'});		
+			}
+			user.remove();
+			console.log('Usuario eliminado con exito');
+		});
+
+});
 
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
 	} else {
 		//req.flash('error_msg','You are not logged in');
-		res.redirect('/login');
+		res.redirect('/');
 	}
 }
 
