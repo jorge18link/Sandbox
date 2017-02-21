afbaa15f1e20d5937099ad2ba02c64dcb2f6c71c
lilibethBm <lilibeth.karen@gmail.com>
2017-02-21 18:06:46 -0500

    cambios menores

--- Sandbox/routes/ejerciciosEstudiante.js
+++ Sandbox/routes/ejerciciosEstudiante.js
@@ -6,5 +6,12 @@ var mongoose= require('mongoose');
 router.get('/',function(req,res){
     res.render('index-Estudiantes')
 })
-
+router.get('/realizar/:id',function(req,res){
+    var id=req.params.id;
+	var busca = {_id: id};
+    ejercicio.findOne(busca,function(err, ejercicio){
+			if(err) throw err;
+			res.render('HacerEjercicio');
+    });    
+})
 module.exports = router;
