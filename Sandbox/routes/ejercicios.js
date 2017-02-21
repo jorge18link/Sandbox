1c6fcca087d390ebc8c03163d0f35356b56450ae
lilibethBm <lilibeth.karen@gmail.com>
2017-02-21 00:23:39 -0500

    .

--- Sandbox/routes/ejercicios.js
+++ Sandbox/routes/ejercicios.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res){
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		//req.flash('error_msg','You are not logged in');
+		res.redirect('/');
+	}
+}
+
+var validacion=function(req,res,next){
+	if(req.user.Rol !="profesor"|| req.user.Rol !="ayudante"){
+  	res.sendStatus(401);
+  	return;
+  }
+  next();	
+}
+
+router.get('/',ensureAuthenticated,validacion, function(req, res){
 	res.send("front end ejercicios")
 });
 
